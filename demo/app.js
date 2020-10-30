45d8ec240f5fcbb035974be488b38b6743113ff7
guesswhozzz <torridtime@gmail.com>
2020-10-30 03:31:09 +0300

    initial commit

--- demo/app.js
+++ demo/app.js
@@ -1,11 +1,10 @@
-var mongoose = require("mongoose");
+const b = `\x1b[42;30m DONE \x1b[0m`;
+const e = `\x1b[41;30m ERROR \x1b[0m`;
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-
-var db = mongoose.connection;
-mongoose.connect("mongodb://127.0.0.1:27017");
 
 app.use(express.static(__dirname + "/public"));
 
@@ -13,8 +12,19 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-console.log(__dirname);
+async function start() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/lasttest", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(port, () => {
+      console.log(`${b} App: work on http://localhost:${port}`);
+    });
+  } catch (e) {
+    console.log(e.message, `Опаньки`);
+    process.exit(1);
+  }
+}
+start();
